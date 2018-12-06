4d1edfcb0d2ff90170204e72f09eff429ed02036
连晓磊 <lianxiaolei@xueleyun.com>
2018-12-06 14:58:37 +0800

    '0'

--- lib/layer_utils/generate_anchors.py
+++ lib/layer_utils/generate_anchors.py
@@ -40,78 +40,97 @@ import numpy as np
 
 def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
                      scales=2 ** np.arange(3, 6)):
-  """
-  Generate anchor (reference) windows by enumerating aspect ratios X
-  scales wrt a reference (0, 0, 15, 15) window.
-  """
+    """
+    Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, 15, 15) window.
+    """
 
-  base_anchor = np.array([1, 1, base_size, base_size]) - 1
-  ratio_anchors = _ratio_enum(base_anchor, ratios)
-  anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
-                       for i in range(ratio_anchors.shape[0])])
-  return anchors
+    base_anchor = np.array([1, 1, base_size, base_size]) - 1
+    # ratio anchors 各种纵横比的框[0.5, 1, 2],
+    # [[-3.5  2.  18.5 13. ]
+    #  [ 0.   0.  15.  15. ]
+    #  [ 2.5 -3.  12.5 18. ]]
+    ratio_anchors = _ratio_enum(base_anchor, ratios)
+    # [[ -84.,  -40.,   99.,   55.],
+    #  [-176.,  -88.,  191.,  103.],
+    #  [-360., -184.,  375.,  199.],
+    #  [ -56.,  -56.,   71.,   71.],
+    #  [-120., -120.,  135.,  135.],
+    #  [-248., -248.,  263.,  263.],
+    #  [ -36.,  -80.,   51.,   95.],
+    #  [ -80., -168.,   95.,  183.],
+    #  [-168., -344.,  183.,  359.]]
+    anchors = np.vstack(
+        [_scale_enum(ratio_anchors[i, :], scales) for i in range(ratio_anchors.shape[0])])
+    return anchors
 
 
 def _whctrs(anchor):
-  """
-  Return width, height, x center, and y center for an anchor (window).
-  """
-
-  w = anchor[2] - anchor[0] + 1
-  h = anchor[3] - anchor[1] + 1
-  x_ctr = anchor[0] + 0.5 * (w - 1)
-  y_ctr = anchor[1] + 0.5 * (h - 1)
-  return w, h, x_ctr, y_ctr
+    """
+    Return width, height, x center, and y center for an anchor (window).
+    """
+    # anchor [ 0  0 15 15]
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    # w h xcenter ycenter 16 16 7.5 7.5
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
 
 
 def _mkanchors(ws, hs, x_ctr, y_ctr):
-  """
-  Given a vector of widths (ws) and heights (hs) around a center
-  (x_ctr, y_ctr), output a set of anchors (windows).
-  """
+    """
+    Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    """
 
-  ws = ws[:, np.newaxis]
-  hs = hs[:, np.newaxis]
-  anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
-                       y_ctr - 0.5 * (hs - 1),
-                       x_ctr + 0.5 * (ws - 1),
-                       y_ctr + 0.5 * (hs - 1)))
-  return anchors
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         y_ctr - 0.5 * (hs - 1),
+                         x_ctr + 0.5 * (ws - 1),
+                         y_ctr + 0.5 * (hs - 1)))
+    return anchors
 
 
 def _ratio_enum(anchor, ratios):
-  """
-  Enumerate a set of anchors for each aspect ratio wrt an anchor.
-  """
+    """
+    Enumerate a set of anchors for each aspect ratio wrt an anchor.
+    """
+    # anchor [ 0  0 15 15]
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    size = w * h
 
-  w, h, x_ctr, y_ctr = _whctrs(anchor)
-  size = w * h
-  size_ratios = size / ratios
-  ws = np.round(np.sqrt(size_ratios))
-  hs = np.round(ws * ratios)
-  anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-  return anchors
+    # size_ratios [512. 256. 128.]
+    size_ratios = size / ratios
+
+    # ws [23. 16. 11.]
+    # hs [12. 16. 22.]
+    ws = np.round(np.sqrt(size_ratios))  # ???
+    hs = np.round(ws * ratios)
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
 
 
 def _scale_enum(anchor, scales):
-  """
-  Enumerate a set of anchors for each scale wrt an anchor.
-  """
+    """
+    Enumerate a set of anchors for each scale wrt an anchor.
+    """
 
-  w, h, x_ctr, y_ctr = _whctrs(anchor)
-  ws = w * scales
-  hs = h * scales
-  anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-  return anchors
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    ws = w * scales
+    hs = h * scales
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
 
 
 if __name__ == '__main__':
-  import time
+    import time
 
-  t = time.time()
-  a = generate_anchors()
-  print(time.time() - t)
-  print(a)
-  from IPython import embed;
+    t = time.time()
+    a = generate_anchors()
+    print(time.time() - t)
+    print(a)
+    from IPython import embed;
 
-  embed()
+    embed()
--- lib/layer_utils/snippets.py
+++ lib/layer_utils/snippets.py
@@ -11,39 +11,58 @@ import tensorflow as tf
 import numpy as np
 from layer_utils.generate_anchors import generate_anchors
 
-def generate_anchors_pre(height, width, feat_stride, anchor_scales=(8,16,32), anchor_ratios=(0.5,1,2)):
-  """ A wrapper function to generate anchors given different scales
-    Also return the number of anchors in variable 'length'
-  """
-  anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
-  A = anchors.shape[0]
-  shift_x = np.arange(0, width) * feat_stride
-  shift_y = np.arange(0, height) * feat_stride
-  shift_x, shift_y = np.meshgrid(shift_x, shift_y)
-  shifts = np.vstack((shift_x.ravel(), shift_y.ravel(), shift_x.ravel(), shift_y.ravel())).transpose()
-  K = shifts.shape[0]
-  # width changes faster, so here it is H, W, C
-  anchors = anchors.reshape((1, A, 4)) + shifts.reshape((1, K, 4)).transpose((1, 0, 2))
-  anchors = anchors.reshape((K * A, 4)).astype(np.float32, copy=False)
-  length = np.int32(anchors.shape[0])
 
-  return anchors, length
+def generate_anchors_pre(height, width, feat_stride, anchor_scales=(8, 16, 32), anchor_ratios=(0.5, 1, 2)):
+    """ A wrapper function to generate anchors given different scales
+      Also return the number of anchors in variable 'length'
+    """
+    anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
+    A = anchors.shape[0]
+    shift_x = np.arange(0, width) * feat_stride
+    shift_y = np.arange(0, height) * feat_stride
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+    shifts = np.vstack((shift_x.ravel(), shift_y.ravel(), shift_x.ravel(), shift_y.ravel())).transpose()
+    K = shifts.shape[0]
+    # width changes faster, so here it is H, W, C
+    anchors = anchors.reshape((1, A, 4)) + shifts.reshape((1, K, 4)).transpose((1, 0, 2))
+    anchors = anchors.reshape((K * A, 4)).astype(np.float32, copy=False)
+    length = np.int32(anchors.shape[0])
+
+    return anchors, length
+
 
 def generate_anchors_pre_tf(height, width, feat_stride=16, anchor_scales=(8, 16, 32), anchor_ratios=(0.5, 1, 2)):
-  shift_x = tf.range(width) * feat_stride # width
-  shift_y = tf.range(height) * feat_stride # height
-  shift_x, shift_y = tf.meshgrid(shift_x, shift_y)
-  sx = tf.reshape(shift_x, shape=(-1,))
-  sy = tf.reshape(shift_y, shape=(-1,))
-  shifts = tf.transpose(tf.stack([sx, sy, sx, sy]))
-  K = tf.multiply(width, height)
-  shifts = tf.transpose(tf.reshape(shifts, shape=[1, K, 4]), perm=(1, 0, 2))
+    # now height=H/16 and width=W/16
+    shift_x = tf.range(width) * feat_stride  # width
+    shift_y = tf.range(height) * feat_stride  # height
+    shift_x, shift_y = tf.meshgrid(shift_x, shift_y)
+    sx = tf.reshape(shift_x, shape=(-1,))
+    sy = tf.reshape(shift_y, shape=(-1,))
+    shifts = tf.transpose(tf.stack([sx, sy, sx, sy]))
+    K = tf.multiply(width, height)
+    shifts = tf.transpose(tf.reshape(shifts, shape=[1, K, 4]), perm=(1, 0, 2))
+    # shifts shape (196, 1, 4)
 
-  anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
-  A = anchors.shape[0]
-  anchor_constant = tf.constant(anchors.reshape((1, A, 4)), dtype=tf.int32)
+    # anchors value
+    # w, h, centerx, centery
+    # [[ -84.,  -40.,   99.,   55.],
+    #  [-176.,  -88.,  191.,  103.],
+    #  [-360., -184.,  375.,  199.],
+    #  [ -56.,  -56.,   71.,   71.],
+    #  [-120., -120.,  135.,  135.],
+    #  [-248., -248.,  263.,  263.],
+    #  [ -36.,  -80.,   51.,   95.],
+    #  [ -80., -168.,   95.,  183.],
+    #  [-168., -344.,  183.,  359.]]
+    anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
+    A = anchors.shape[0]  # 9
+    anchor_constant = tf.constant(anchors.reshape((1, A, 4)), dtype=tf.int32)
+    # shape [1, 9 ,4]
 
-  length = K * A
-  anchors_tf = tf.reshape(tf.add(anchor_constant, shifts), shape=(length, 4))
-  
-  return tf.cast(anchors_tf, dtype=tf.float32), length
+    length = K * A
+    anchors_tf = tf.reshape(tf.add(anchor_constant, shifts), shape=(length, 4))
+    # tf.reshape(tf.add(anchor_constant, shifts)
+    # shape (196, 9, 4) 横纵各走16个，每个9个anchor
+    # anchors_tf shape (1764, 4)
+
+    return tf.cast(anchors_tf, dtype=tf.float32), length
